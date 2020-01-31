4d0b0acfe147cc7954bcb6afd58f4e9290877d1a
Rory T. Giszter <tuc73313@temple.edu>
2020-01-31 16:10:11 -0500

    Created Shape Class

---
+++ src/Shape.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tuc73313
+ */
+public class Shape {
+    private  String name;
+    public Shape(String name) { 
+        this.name = name; 
+    }
+    
+    /** returns the name of the shape
+     * @return shape's name */ 
+    public String getName() { 
+        return name; 
+    }
+    
+    /** returns the area of the shape
+     * @return shape's area */ 
+    public double getArea() { 
+        return 0.0; 
+    }
+    
+    public void printDimensions(){ 
+        System.out.println("No Dimensions"); 
+    }
+}
